e506d1bbf273bedd153ac8f99c3b13efff97640d
Andrew Pigram <andrew.pigram@toukanlabs.com>
2021-11-04 23:54:13 +1100

    Added Map Controller script to handle unit/building management. Added placeholder for Worker Bee (will be replaced later). Added Unit and Building components - both implement the ISelectable interface to allow them to be selected. Units can move to a target location.

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour, ISelectable
+{
+    public string unitType;
+    public float moveSpeed;
+    Vector3 target = Vector3.zero;
+    bool moving = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving)
+        {
+            Vector3 newPosition = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+
+            transform.position = newPosition;
+
+            if (transform.position == target)
+            {
+                target = Vector3.zero;
+                moving = false;
+                DidStopMoving();
+            }
+        }
+    }
+
+    public bool IsMovable()
+    {
+        return true;
+    }
+
+    public void MoveToPosition(Vector3 position)
+    {
+        target = position;
+        target.z = transform.position.z;
+        moving = true;
+    }
+
+    public void OnSelect()
+    {
+        Debug.Log($"{unitType} selected");
+    }
+
+    public void DidStopMoving()
+    {
+        Debug.Log($"{unitType} has reached its destination.");
+    }
+}
