0f0939c24f669b73daaa93e308de0ecb7b3d60f1
Andrew Pigram <andrew.pigram@toukanlabs.com>
2021-11-09 20:10:16 +1100

    Added EnemyWave class to represent a wave of enemies. Once 5 enemies have been added to a wave, the enemies in the wave will launch an attack on the hive. If a unit or building falls within a certain radius of a unit (currently only implemented for enemies), the units will now target the closest of the units/buildings in the radius. If an enemy kills its target, it will then re-target the hive unless another unit/building falls within its radius. Increased turn speed for wasp and worker bees to 180 degrees/second for more natural turning. References to the Hive grid and hive position are now static singletons to improve performance. Coroutines are now assigned to objects to allow stopping of individual Coroutines (Coroutines only stop at the end of the frame so StopAllCoroutines is not ideal when trying to stop all coroutines and then execute another Coroutine afterwards). Added attackRate and baseDamage values to UnitType. These dictate how many attacks the unit will make per second and the base damage done by each attack. Units will now fall to the ground when dead, and will then be destroyed after 5 seconds.

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -12,6 +12,8 @@ public class EnemySpawner : MonoBehaviour
     private bool spawningEnabled = false;
     private float nextSpawnTime = 0.0f;
 
+    EnemyWave currentWave;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,17 +29,28 @@ public class EnemySpawner : MonoBehaviour
             float currentSpawnRate = spawnRate.Evaluate(Time.time / 60f);
             nextSpawnTime = Time.time + (60f / currentSpawnRate);
         }
+        if (currentWave.Attacking)
+        {
+            // Build a new enemy wave. I set to null to signal garbage collection on the previous wave before initialising the next one.
+            currentWave = null;
+            currentWave = new EnemyWave(5);
+        }
     }
 
     private void Spawn()
     {
         var spawnedInstance = Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+        spawnedInstance.AssignToWave(currentWave);
 
         if (!rallyPoint.IsGridFull)
         {
             var rallyGridCell = rallyPoint.GetNextAvailableCell();
             rallyGridCell.OccupyCell();
-            spawnedInstance.Move(rallyGridCell.Position, rallyPoint.transform.rotation);
+
+            // This rotation will turn the enemy to face the direction it is moving rather than having it face the same way as the rally point grid.
+            Vector3 faceDirection = -(spawnedInstance.transform.position - rallyGridCell.Position).normalized;
+            faceDirection.y = 0;
+            spawnedInstance.Move(rallyGridCell.Position, Quaternion.FromToRotation(spawnedInstance.transform.forward, faceDirection));
         }
         else
         {
@@ -48,6 +61,7 @@ public class EnemySpawner : MonoBehaviour
     public void EnableSpawning()
     {
         spawningEnabled = true;
+        currentWave = new EnemyWave(5);
     }
 
     public void DisableSpawning()
---
+++ Assets/Scripts/Enemy/EnemyWave.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyWave
+{
+    public List<Enemy> enemies;
+    public int waveSize;
+    public bool Attacking { get; private set; } = false;
+    // Use this for initialization
+    public EnemyWave(int size)
+    {
+        waveSize = size;
+        enemies = new List<Enemy>();
+    }
+
+    // Update is called once per frame
+    public void AddUnitToWave(Enemy e)
+    {
+        enemies.Add(e);
+        if (enemies.Count == waveSize)
+        {
+            LaunchAttack();
+        }
+    }
+
+    void LaunchAttack()
+    {
+        foreach (var e in enemies)
+        {
+            e.TargetHive();
+        }
+        Attacking = true;
+    }
+}
