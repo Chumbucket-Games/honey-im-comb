60e94ac525801687a668aaa35732d0f550b5ec2a
Matthew Pigram <mattpigram90@gmail.com>
2021-11-15 20:31:00 +1100

    Added a script to handle starting a new game from the main menu as well as exiting the game

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnNewGameButtonPressed()
     {
-        
+        SceneManager.LoadScene(Constants.Scenes.TheHive);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnExitButtonPressed()
     {
-        
+        Application.Quit();
     }
 }
---
+++ Assets/Scripts/Utility/Constants.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants
+{
+    public struct Scenes
+    {
+        public const int MainMenu = 0;
+        public const int TheHive = 1;
+    }
+}
